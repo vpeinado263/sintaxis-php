8be35a23fdbae9f30da2ce1e156d678b601e39c3
Victor Peinado <vpeinado263@gmail.com>
2026-07-16 15:19:54 -0300

    Configurar script edad.php

--- edad.php
+++ edad.php
@@ -1,9 +1,26 @@
 <?php
 
-echo "<h1>ENFER-DATS</h1>";
+echo "<h1>ENFER-DATS Script: Validación de Edad</h1>";
 
 echo "<h2>Trabajo Práctico - Semana 10</h2>";
 
 echo "<hr>";
+
+// Variables
+$paciente = "NN";
+$edad = 72;
+
+// Salida de datos
+echo "<h3>Validación de Edad del Paciente</h3>";
+
+echo "Paciente: $paciente <br>";
+echo "Edad: $edad años <br><br>";
+
+// Estructura condicional
+if ($edad >= 18) {
+    echo "<strong>Resultado:</strong> Paciente mayor de edad.";
+} else {
+    echo "<strong>Resultado:</strong> Paciente menor de edad.";
+}
 
 ?>
