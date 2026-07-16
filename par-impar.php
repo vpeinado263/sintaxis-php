ce256d42764ece34ed5d7460931d04bd2458ff82
Victor Peinado <vpeinado263@gmail.com>
2026-07-16 15:53:15 -0300

    Agregar script par-impar.php

--- par-impar.php
+++ par-impar.php
@@ -1,9 +1,33 @@
 <?php
 
-echo "<h1>ENFER-DATS</h1>";
+echo "<h1>ENFER-DATS Trabajo Práctico - Semana 10</h1>";
 
-echo "<h2>Trabajo Práctico - Semana 10</h2>";
+echo "<h2>Script: Validación de habitación y cama</h2>";
 
 echo "<hr>";
+
+// Variables
+$habitacion = 205;
+$cama = 18;
+
+echo "<h3>Validación de Habitación y Cama</h3>";
+
+// Validación de habitación
+echo "<strong>Habitación:</strong> $habitacion <br>";
+
+if ($habitacion % 2 == 0) {
+    echo "Resultado: Habitación par.<br><br>";
+} else {
+    echo "Resultado: Habitación impar.<br><br>";
+}
+
+// Validación de cama
+echo "<strong>Cama Nº:</strong> $cama <br>";
+
+if ($cama % 2 == 0) {
+    echo "Resultado: Número par.";
+} else {
+    echo "Resultado: Número impar.";
+}
 
 ?>
