5ce6222d32bf573ea033e2283b02ebbaf5beeb83
Victor Peinado <vpeinado263@gmail.com>
2026-07-16 20:11:58 -0300

    Agreggar cambios

--- promedio.php
+++ promedio.php
@@ -1,26 +1,23 @@
 <?php
-
-echo "<h1>ENFER-DATS  Script: Promedio de Insumos</h1>";
-
+echo "<h1>ENFER-DATS</h1>";
 echo "<h2>Trabajo Práctico - Semana 10</h2>";
+echo "<h3>Script: Promedio de Insumos</h3>";
 
 echo "<hr>";
-
-// Variables
+//Uso de variables
 $turnoManana = 18;
 $turnoTarde = 22;
 $turnoNoche = 20;
 
-// Operación matemática
+// Operación matemática básica
 $promedio = ($turnoManana + $turnoTarde + $turnoNoche) / 3;
 
-// Salida de datos
-echo "<h3>Promedio de insumos utilizados por turno</h3>";
+//
+echo "<h4>Promedio de insumos utilizados por turno</h4>";
 
 echo "Turno Mañana: $turnoManana insumos <br>";
 echo "Turno Tarde: $turnoTarde insumos <br>";
 echo "Turno Noche: $turnoNoche insumos <br><br>";
-
 echo "<strong>RESULTADO: $promedio</strong>";
 
 
